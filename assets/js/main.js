743f58781824c4ee3da9fe9e6b27766624684796
Adrien bouchet <adrien.bouchet.974@gmail.com>
2022-06-15 16:17:10 +0200

    Ajout d'un style display sur la div.

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,15 +9,18 @@
 const tout = document.querySelector("#tout");
 const credit = document.querySelector("#credit");
 const debit = document.querySelector("#debit");
+const main = document.querySelector("#main");
 
 function active() {
     credit.addEventListener("click", () => {
         credit.classList.add("active");
         credit.classList.add("fade-in-top");
+        main.classList.add("fade-in-top");
         credit.addEventListener("animationend", () => {
             credit.classList.remove("fade-in-top")
         });
         if(credit.classList.contains("active")){
+            main.style.display = "none";
             tout.classList.remove("active");
             debit.classList.remove("active");
         }
@@ -25,10 +28,12 @@ function active() {
     debit.addEventListener("click", () => {
         debit.classList.add("active");
         debit.classList.add("fade-in-right");
+        main.classList.add("fade-in-right");
         debit.addEventListener("animationend", () => {
             debit.classList.remove("fade-in-right")
         });
         if(debit.classList.contains("active")){
+            main.style.display = "none";
             tout.classList.remove("active");
             credit.classList.remove("active");
         }
@@ -36,10 +41,12 @@ function active() {
     tout.addEventListener("click", () => {
         tout.classList.add("active");
         tout.classList.add("fade-in-left");
+        main.classList.add("fade-in-left");
         tout.addEventListener("animationend", () => {
             tout.classList.remove("fade-in-left")
         });
         if(tout.classList.contains("active")){
+            main.style.display = "";
             debit.classList.remove("active");
             credit.classList.remove("active");
         }
@@ -49,3 +56,4 @@ active();
 
 
 const operator = document.querySelector("#operator");
+console.log(main)
