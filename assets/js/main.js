0a8a922ec97816510028ebaf8294bd7595db3642
Adrien bouchet <adrien.bouchet.974@gmail.com>
2022-06-15 14:43:24 +0200

    Ajout de fonction pour changer la couleur du bouton si il est actif.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,36 @@
-console.log("bonjour")
+// const result = datapoints.filter(check_value);
+
+// function check_value(age) {
+//     return age >= 1200 | age == 775;
+// }
+
+// console.log(result)
+
+const tout = document.querySelector("#tout");
+const credit = document.querySelector("#credit");
+const debit = document.querySelector("#debit");
+
+function active_credit() {
+    credit.addEventListener("click", () => {
+        credit.classList.add("active");
+        if(credit.classList.contains("active")){
+            tout.classList.remove("active");
+            debit.classList.remove("active");
+        }
+    })
+    debit.addEventListener("click", () => {
+        debit.classList.add("active");
+        if(debit.classList.contains("active")){
+            tout.classList.remove("active");
+            credit.classList.remove("active");
+        }
+    })
+    tout.addEventListener("click", () => {
+        tout.classList.add("active");
+        if(tout.classList.contains("active")){
+            debit.classList.remove("active");
+            credit.classList.remove("active");
+        }
+    })
+}
+active_credit();
