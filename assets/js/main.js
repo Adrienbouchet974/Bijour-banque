af8efbd3d3a50c99d893cf21f8b75805597fd018
Adrien bouchet <adrien.bouchet.974@gmail.com>
2022-06-21 17:03:21 +0200

    les commenatires ont été supprimé.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,71 +1,71 @@
-// const tout = document.querySelector("#tout");
-// const credit = document.querySelector("#credit");
-// const debit = document.querySelector("#debit");
-// const main = document.querySelector("#main");
-// const op_debit = main.querySelectorAll("div.operation.debit");
-// const op_credit = main.querySelectorAll("div.operation.credit");
+const tout = document.querySelector("#tout");
+const credit = document.querySelector("#credit");
+const debit = document.querySelector("#debit");
+const main = document.querySelector("#main");
+const op_debit = main.querySelectorAll("div.operation.debit");
+const op_credit = main.querySelectorAll("div.operation.credit");
 
-// function active() {
-//     credit.addEventListener("click", () => {
-//         credit.classList.add("active");
-//         credit.classList.add("fade-in-fwd");
-//         main.classList.add("fade-in-fwd");
-//         op_debit.forEach(element => {
-//             element.style.display = "none"
-//         });
-//         op_credit.forEach(element => {
-//             element.style.display = ""
-//         });
-//         credit.addEventListener("animationend", () => {
-//             credit.classList.remove("fade-in-fwd");
-//             main.classList.remove("fade-in-fwd");
-//         });
-//         if(credit.classList.contains("active")){
-//             tout.classList.remove("active");
-//             debit.classList.remove("active");
-//         }
-//     })
-//     debit.addEventListener("click", () => {
-//         debit.classList.add("active");
-//         debit.classList.add("fade-in-right");
-//         main.classList.add("fade-in-right");
-//         op_credit.forEach(element => {
-//             element.style.display = "none"
-//         });
-//         op_debit.forEach(element => {
-//             element.style.display = ""
-//         });
-//         debit.addEventListener("animationend", () => {
-//             debit.classList.remove("fade-in-right");
-//             main.classList.remove("fade-in-right");
-//         });
-//         if(debit.classList.contains("active")){
-//             tout.classList.remove("active");
-//             credit.classList.remove("active");
-//         }
-//     })
-//     tout.addEventListener("click", () => {
-//         tout.classList.add("active");
-//         tout.classList.add("fade-in-left");
-//         main.classList.add("fade-in-left");
-//         op_credit.forEach(element => {
-//             element.style.display = "";
-//         });
-//         op_debit.forEach(value => {
-//             value.style.display = "";
-//         });
-//         tout.addEventListener("animationend", () => {
-//             tout.classList.remove("fade-in-left");
-//             main.classList.remove("fade-in-left");
-//         });
-//         if(tout.classList.contains("active")){
-//             main.style.display = "";
-//             debit.classList.remove("active");
-//             credit.classList.remove("active");
-//         }
-//     })
-// }
-// active();
+function active() {
+    credit.addEventListener("click", () => {
+        credit.classList.add("active");
+        credit.classList.add("fade-in-fwd");
+        main.classList.add("fade-in-fwd");
+        op_debit.forEach(element => {
+            element.style.display = "none"
+        });
+        op_credit.forEach(element => {
+            element.style.display = ""
+        });
+        credit.addEventListener("animationend", () => {
+            credit.classList.remove("fade-in-fwd");
+            main.classList.remove("fade-in-fwd");
+        });
+        if(credit.classList.contains("active")){
+            tout.classList.remove("active");
+            debit.classList.remove("active");
+        }
+    })
+    debit.addEventListener("click", () => {
+        debit.classList.add("active");
+        debit.classList.add("fade-in-right");
+        main.classList.add("fade-in-right");
+        op_credit.forEach(element => {
+            element.style.display = "none"
+        });
+        op_debit.forEach(element => {
+            element.style.display = ""
+        });
+        debit.addEventListener("animationend", () => {
+            debit.classList.remove("fade-in-right");
+            main.classList.remove("fade-in-right");
+        });
+        if(debit.classList.contains("active")){
+            tout.classList.remove("active");
+            credit.classList.remove("active");
+        }
+    })
+    tout.addEventListener("click", () => {
+        tout.classList.add("active");
+        tout.classList.add("fade-in-left");
+        main.classList.add("fade-in-left");
+        op_credit.forEach(element => {
+            element.style.display = "";
+        });
+        op_debit.forEach(value => {
+            value.style.display = "";
+        });
+        tout.addEventListener("animationend", () => {
+            tout.classList.remove("fade-in-left");
+            main.classList.remove("fade-in-left");
+        });
+        if(tout.classList.contains("active")){
+            main.style.display = "";
+            debit.classList.remove("active");
+            credit.classList.remove("active");
+        }
+    })
+}
+active();
 
 
 // const operator = document.querySelector("#operator");
